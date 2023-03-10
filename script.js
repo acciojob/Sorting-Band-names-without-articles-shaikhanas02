f48ce92da9254232b208fc60ca1b52913cfdba29
shaikhanas02 <122043549+shaikhanas02@users.noreply.github.com>
2023-03-10 16:27:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
+//your code here
 
+function strip(article, bandName) {
+return bandName.replace(article, '').trim();
+}
+const articles = ['a', 'an', 'the'];
+const bands = ['The Beatles', 'Led Zeppelin', 'Aerosmith', 'The Who', 'Pink Floyd'];
+const sortedBands = bands.sort((a, b) => {
+const strippedA = articles.reduce(strip.bind(null, a), a);
+const strippedB = articles.reduce(strip.bind(null, b), b);
+return strippedA.localeCompare(strippedB);
+});
+const ul = document.querySelector('#bands');
+ul.innerHTML = sortedBands.map(band => <li>${band}</li>).join('');
